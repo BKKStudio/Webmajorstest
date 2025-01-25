05066e41fa92af9af904f523cc0dffe18c744e29
First Last <seksak22180@gmail.com>
2025-01-25 18:12:22 +0700

    fix stlye responsive

--- app/Pages/Thesis/page.jsx
+++ app/Pages/Thesis/page.jsx
@@ -154,7 +154,7 @@ export default function Thesis() {
               <select
                 name="academicYear"
                 id="academicYear"
-                className="text-center bg-white text-gray-600 text-xs w-60 rounded-xl py-2 "
+                className="text-center bg-white text-gray-600 text-xs w-72 rounded-xl py-2 "
                 value={academicYear}
                 onChange={(e) => handleFilteredYear(e.target.value)}
               >
@@ -187,7 +187,7 @@ export default function Thesis() {
                     </div>
                     <div className="flex flex-col gap-3 justify-between items-center">
                       <div className="flex flex-col gap-3 justify-centers items-center">
-                        <div className="font-bold text-xl bg-red-400  w-full h-28 mt-10 flex justify-center items-center  text-center">
+                        <div className="font-bold text-xl px-3 w-full h-28 mt-10 flex justify-center items-center  text-center">
                           {item.TitleProject}
                         </div>
                         <div className="w-3/4 h-0.5 bg-black"></div>
